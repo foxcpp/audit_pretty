d8f7362429650bfdcd25834130e453ff3f74f70d
hkettu <hkettu@disroot.org>
2018-04-03 19:40:50 +0000

    Bunch of new options and other minor improvements
    Add --since, --until options.
    Separate options into groups.
    Move version into __init__.py.
    Fix RuntimeWarning when running using python -m.
    Provide better formatting for messages with unknown type.
    Fix handling of ptrace DENIED in AppArmor pretty printer.
    Document how to add support for new message types.

--- audit_pretty/__main__.py
+++ audit_pretty/__main__.py
@@ -21,7 +21,7 @@ def parse_message(line: str) -> dict:
     if trimmed.startswith('['):
         # Reading from dmesg, strip timestamp and 'audit:' prefix.
         trimmed = re.sub(r'^\[\d+\.\d+\] audit: ', '', trimmed) 
-    match = re.fullmatch(r'(type=\d+) audit\((\d+)\.\d+:\d+\): (.+)', trimmed)
+    match = re.fullmatch(r'(type=[0-9_A-Z]+) (?:msg=)?audit\((\d+)\.\d+:\d+\): (.+)', trimmed)
     if match is None:
         return None
     trimmed = match.group(1) + ' ' + 'time=' + match.group(2) + ' ' + match.group(3)
@@ -42,36 +42,70 @@ def parse_message(line: str) -> dict:
 def setup_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
             description='Linux Auditing System logs pretty printer',
-            epilog='Known message types: ' + ','.join(pretty_printers.keys()))
+            epilog='Known message types: ' + ','.join(pretty_printers.keys()),
+            add_help=False)
 
-    parser.add_argument('-v', '--verbose', help='Include fields hidden by default.',
-                        action='store_true')
+    dummy = parser.add_argument_group()
+    dummy.add_argument('-h', '--help', help='show this text and exit', action='store_true')
 
-    parser.add_argument('-m', '--merge', help='Print similar events only once.',
-                        action='store_true')
-    parser.add_argument('-c', '--count', help='Count similar events. Implies --merge. Will not work with dmesg -w or tail -f.',
-                        action='store_true')
+    filters = parser.add_argument_group('Message filtering')
+    filters.add_argument('-s', '--since', help='show only entries since this UNIX timestamp',
+                        action='store', default=0, type=int)
+    filters.add_argument('-u', '--until', help='show only entries until this UNIX timestamp',
+                        action='store', default=2**64-1, type=int)
 
-    parser.add_argument('--color', help='Use ANSI escape codes to color output.',
-                        action='store_true', default=sys.stdout.isatty())
-    parser.add_argument('--no-color', help='Don\'t use ANSI escape codes to color output.',
-                        action='store_false', dest='color')
-
-    parser.add_argument('--hide-unknown', help='Hide messages with unknown type instead of printing in raw format.',
-                        action='store_true')
-
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-e', '--exclude', help='Skip messages with this type. Can be specified multiple times.',
+    group = filters.add_mutually_exclusive_group()
+    group.add_argument('-e', '--exclude', help='skip messages with this type; can be specified multiple times',
                        action='append', choices=pretty_printers.keys(), metavar='TYPE', default=[])
-    group.add_argument('-i', '--only', help='Print only messages with this type. Can be specified multiple times.',
+    group.add_argument('-i', '--only', help='print only messages with this type; can be specified multiple times',
                        action='append', choices=pretty_printers.keys(), metavar='TYPE', default=[])
+
+    filters.add_argument('--hide-unknown', help='hide messages with unknown type instead of printing in raw format',
+                        action='store_true')
+
+    formatting = parser.add_argument_group('Output formatting')
+
+    formatting.add_argument('-v', '--verbose', help='include fields hidden by default',
+                        action='store_true')
+
+    formatting.add_argument('-m', '--merge', help='print similar events only once',
+                            action='store_true')
+    formatting.add_argument('-c', '--count', help='count similar events; implies --merge; will not work with dmesg -w or tail -f.',
+                            action='store_true')
+
+    formatting.add_argument('--color', help='use ANSI escape codes to color output.',
+                            action='store_true', default=sys.stdout.isatty())
+    formatting.add_argument('--no-color', help='don\'t use ANSI escape codes to color output.',
+                            action='store_false', dest='color')
 
     return parser
 
 
+def should_print(args, msg) -> bool:
+    # Yes, I want to write this function exactly this way because it's easier to read.
+    if msg is None:
+        return False
+
+    if msg['time'] < args.since or msg['time'] > args.until:
+        return False
+
+    if len(args.exclude) != 0 and msg['type'] in args.exclude or\
+       len(args.only) != 0 and msg['type'] not in args.only:
+        return False
+
+    if msg['type'] not in pretty_printers and args.hide_unknown:
+        return False
+
+    return True
+
+
 def main():
-    global args
-    args = setup_argparse().parse_args()
+    parser = setup_argparse()
+    args = parser.parse_args()
+    if args.help:
+        parser.print_help()
+        return
+
     if args.count:
         args.merge = True
     if not args.color:
@@ -82,42 +116,29 @@ def main():
         already_seen = dict()
         for line in sys.stdin:
             msg = parse_message(line)
-            if msg is None:
+
+            if type(msg['type']) == int:
+                # dmesg log contains messages with numeric type.
+                # Remap to string type for further processing.
+                msg['type'] = string_types[msg['type']]
+
+            if not should_print(args, msg):
                 continue
-            try:
-                if type(msg['type']) == int:
-                    # dmesg log contains messages with numeric type.
-                    # Remap to string type for further processing.
-                    msg['type'] = string_types[msg['type']]
 
-                if len(args.exclude) != 0 and msg['type'] in args.exclude or\
-                   len(args.only) != 0 and msg['type'] not in args.only:
-                    continue
-
-                main_info = main_info_filters[msg['type']](msg)
-                result = pretty_printers[msg['type']](msg)
-                if result is None and not args.hide_unknown:
-                    print('Failed to format message, printing as is.')
-                    print(line)
-                if args.merge:
-                    hashable_info = FrozenDict(main_info)
-                    if hashable_info not in already_seen.keys():
-                        if not args.count:
-                            print(result)
-                    already_seen[hashable_info] = already_seen.get(hashable_info, 0) + 1
-                else:
-                    print(result)
-            except KeyError:
-                if len(args.only) == 0 and not args.hide_unknown:
-                    print('Unknown message type, printing as is.')
-                    print(line)
+            main_info = main_info_filters[msg['type']](msg)
+            result = pretty_printers[msg['type']](msg)
+            if args.merge: # Count only if we need it.
+                hashable_info = FrozenDict(main_info)
+                if hashable_info not in already_seen.keys():
+                    if not args.count: # We will print all messages later.
+                        print(result)
+                already_seen[hashable_info] = already_seen.get(hashable_info, 0) + 1
+            else:
+                print(result)
 
         if args.count:
             for info, count in already_seen.items():
                 result = pretty_printers[info['type']](info, suffix='(' + str(count) + ')')
-                if result is None and not args.hide_unknown:
-                    print('Failed to format message, printing as is.')
-                    print(line)
                 print(result)
     except KeyboardInterrupt:
         return
--- audit_pretty/parsers.py
+++ audit_pretty/parsers.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
 from audit_pretty.pretty_utils import pretty_helper
 
 # How to add support for new message type:
 # 1. 
 #
 
-def apparmor_pretty(msg, color=False, suffix='') -> str:
+def apparmor_pretty(msg, suffix='') -> str:
     if msg['apparmor'] == 'DENIED':
         return pretty_helper(
                 'AppArmor policy violation',
@@ -12,10 +13,10 @@ def apparmor_pretty(msg, color=False, suffix='') -> str:
                 urgency='warn',
                 suffix=suffix,
                 info={
-                    'Operation': msg['operation'],
-                    'Profile': msg['profile'],
-                    'Target': msg['name'],
-                    'Denied mask': msg['denied_mask']
+                    'Operation': msg.get('operation', '?'),
+                    'Profile': msg.get('profile', '?'),
+                    'Target': msg.get('name', msg.get('peer', '?')),
+                    'Denied mask': msg.get('denied_mask', '?')
                 },
                 extra_info={
                     'Requested mask': msg.get('requested_mask', '?'),
@@ -25,26 +26,55 @@ def apparmor_pretty(msg, color=False, suffix='') -> str:
                 })
     else:
         print('Unknown AppArmor message type, printing as is.')
-        return None
+        return default_pretty_printer(msg, suffix)
 
 
 def apparmor_main_info(msg) -> dict:
     m = msg.copy()
+    def del_if_present(key):
+        if key in m:
+            del m[key]
     if m['apparmor'] == 'DENIED':
         del m['time']
         del m['pid']
-        del m['fsuid']
-        del m['ouid']
         del m['comm']
+        # Don't present if event is not related to file system (ptrace comes to mind).
+        del_if_present('fsuid')
+        del_if_present('ouid')
     return m
 
 
-pretty_printers: dict = {
-    'AVC': apparmor_pretty
-}
+def default_pretty_printer(msg, suffix='') -> str:
+    return pretty_helper(
+        'Unknown message type (type=' + msg['type'] + ')',
+        time_=msg.get('time', 0),
+        urgency='warn',
+        suffix=suffix,
+        info=dict(((k, v) for k, v in msg.items() if k not in {'time', 'type'})),
+        extra_info={}
+    )
 
 
-main_info_filters: dict = {
-    'AVC': apparmor_main_info
-}
+def default_info_filter(msg) -> dict:
+    m = msg.copy()
+
+    def del_if_present(key):
+        if key in m:
+            del m[key]
+
+    del_if_present('time')
+    del_if_present('pid')
+    del_if_present('fsuid')
+    del_if_present('comm')
+    return msg
+
+
+pretty_printers: dict = defaultdict(lambda: default_pretty_printer,
+    AVC=apparmor_pretty
+)
+
+
+main_info_filters: dict = defaultdict(lambda: default_info_filter,
+    AVC=apparmor_main_info
+)
 
