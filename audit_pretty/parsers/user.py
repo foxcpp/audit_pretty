ee4ca74ff086f38e0f58a90a1b72c6cf54f4b8fd
fox.cpp <fox.cpp@disroot.org>
2018-04-18 21:11:15 +0000

    Add support for USER_LOGOUT, ADD_USER and DEL_USER

--- audit_pretty/parsers/user.py
+++ audit_pretty/parsers/user.py
@@ -38,6 +38,11 @@ def user_login(msg, suffix='') -> str:
     return generic_user_event('User logged in', msg, suffix)
 
 
+@pretty_printer('USER_LOGOUT', 1113)
+def user_login(msg, suffix='') -> str:
+    return generic_user_event('User logged out', msg, suffix)
+
+
 @pretty_printer('USER_END', 1106)
 def user_end(msg, suffix='') -> str:
     return generic_user_event('User session ended', msg, suffix)
@@ -52,8 +57,8 @@ def user_cmd(msg, suffix) -> str:
         urgency='info',
         suffix=suffix,
         info={
-            'Executor\'s UID': decode_uid(msg['auid'], default='UNKNOWN-USER') + ' (' + str(msg['auid']) + ')',
-            'Working directory': pam_msg['cwd'],
+            'Executor\'s UID': decode_uid(msg.get('auid'), str(msg['auid'])) if 'auid' in msg else None,
+            'Working directory': pam_msg.get('cwd'),
             'Command': decode_unsafe_hex(str(pam_msg['cmd'])),
             'Session': msg.get('ses'),
             'Success': 'Yes' if pam_msg.get('res') == 'success' else 'No'
@@ -76,8 +81,79 @@ def user_auth(msg, suffix='') -> str:
     return generic_user_event('User authentication', msg, suffix)
 
 
+@pretty_printer('ADD_USER', 1114)
+def add_user(msg, suffix=''):
+    pam_msg = split_message(msg['msg'], quotes='"')
+    return format_helper(
+        title='New user created',
+        timestamp=datetime.fromtimestamp(msg['time']) if 'time' in msg else None,
+        urgency='info',
+        suffix=suffix,
+        info={
+            'Created by': decode_uid(msg.get('auid')) if 'auid' in msg else None,
+            'New user ID': decode_uid(pam_msg['id'], str(pam_msg['id'])),
+            'Command': pam_msg.get('exe', pam_msg.get('cmd')),
+            'Session': msg.get('ses'),
+            'Success': 'Yes' if pam_msg.get('res') == 'success' else 'No'
+        },
+        extra_info={
+            'Process ID': msg.get('pid'),
+            'Hostname': pam_msg.get('hostname') if pam_msg.get('hostname') != '?' else None,
+            'Address': pam_msg.get('addr') if pam_msg.get('addr') != '?' else None,
+            'Terminal': pam_msg.get('terminal') if pam_msg.get('terminal') != '?' else None
+        })
+
+
+@pretty_printer('DEL_USER', 1115)
+def del_user(msg, suffix=''):
+    pam_msg = split_message(msg['msg'], quotes='"')
+    return format_helper(
+        title='User deleted',
+        timestamp=datetime.fromtimestamp(msg['time']) if 'time' in msg else None,
+        urgency='info',
+        suffix=suffix,
+        info={
+            'Deleted by': decode_uid(msg.get('auid')) if 'auid' in msg else None,
+            # I love consistency of PAM audit logs.
+            'Deleted user ID': pam_msg.get('id') if pam_msg.get('res') == 'success' else pam_msg.get('acct'),
+            'Command': pam_msg.get('exe', pam_msg.get('cmd')),
+            'Session': msg.get('ses'),
+            'Success': 'Yes' if pam_msg.get('res') == 'success' else 'No'
+        },
+        extra_info={
+            'Process ID': msg.get('pid'),
+            'Hostname': pam_msg.get('hostname') if pam_msg.get('hostname') != '?' else None,
+            'Address': pam_msg.get('addr') if pam_msg.get('addr') != '?' else None,
+            'Terminal': pam_msg.get('terminal') if pam_msg.get('terminal') != '?' else None
+        })
+
+
+@main_info_filter('ADD_USER', 'DEL_USER')
+def user_crud_info(msg):
+    m = msg.copy()
+    pam = split_message(m['msg'], quotes='"')
+
+    def rem(d, key):
+        if key in d:
+            del d[key]
+
+
+    rem(m, 'id')
+    rem(m, 'time')
+    rem(m, 'msg')
+    rem(m, 'ses')
+    rem(pam, 'cwd')
+    rem(pam, 'op')
+    rem(pam, 'exe')
+    rem(pam, 'terminal')
+
+    m['msg'] = ' '.join(['{}="{}"'.format(k, v) for k, v in pam.items()])
+    return m
+
+
 @main_info_filter('USER_START', 1105, 'USER_LOGIN', 1112, 'USER_END', 1106,
-                  'USER_CMD', 1123, 'USER_ACCT', 1101, 'USER_AUTH', 1100)
+                  'USER_CMD', 1123, 'USER_ACCT', 1101, 'USER_AUTH', 1100,
+                  'USER_LOGOUT', 1113)
 def user_main_info(msg):
     m = msg.copy()
     pam = split_message(m['msg'], quotes='"')
